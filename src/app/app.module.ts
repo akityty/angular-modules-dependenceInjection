faf700c4c5402f34706937e1c76509235c99498a
akityty <congnv.9803@gmail.com>
2019-10-31 11:20:30 +0700

    img-slider

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {ImageGalleryModule} from './image-gallery/image-gallery.module';
 import {GalleryConfig} from './image-gallery/token';
+import {ImgSliderModule} from './img-slider/img-slider.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import {GalleryConfig} from './image-gallery/token';
   ],
   imports: [
     BrowserModule,
-    ImageGalleryModule
+    ImageGalleryModule,
+    ImgSliderModule,
   ],
   providers: [{provide: GalleryConfig, useValue: 3}],
   bootstrap: [AppComponent]
